f93d07748e77dcabb491fdb23bb15a08f200bad3
HYEJI <58590260+h-y-e-j-i@users.noreply.github.com>
2022-01-13 14:56:22 +0900

    Update rock_scissors_paper.py

--- 03 REINFORCEMENT LEARNING/rock_scissors_paper.py
+++ 03 REINFORCEMENT LEARNING/rock_scissors_paper.py
@@ -14,28 +14,23 @@ class RockScissorsPaper(gym.Env):
         self.me = None
 
         # [내가 낸 손 모양, 컴퓨터가 낸 손모양]
-        self.victory = [[1, 3], [2, 0], [0, 1]]
-        self.fail = [[3, 1], [0, 2], [1, 0]]
+        self.victory = [[1, 2], [2, 0], [0, 1]]
+        self.fail = [[2, 1], [0, 2], [1, 0]]
 
         self.game_count = 0  
         self.episode_count = 0
-        self.results = dict() # 결과
 
-        action_space = spaces.Discrete(3) # 0은 주먹, # 1은 가위, 2는 보자기
-        observation_space = spaces.Discrete(3) # 0은 주먹, # 1은 가위, 2는 보자기
+        self.action_space = spaces.Discrete(3) # 0은 주먹, # 1은 가위, 2는 보자기
+        self.observation_space = spaces.Discrete(3) # 0은 주먹, # 1은 가위, 2는 보자기
 
     def reset(self):
-        self.computer = random.random(0, 4) # 컴퓨터가 초기에 낼 손 모양
-        observation = self.computer
+        self.computer = random.randrange(0, 3) # 컴퓨터가 초기에 낼 손 모양
+        self.observation = self.computer
+
         self.episode_count += 1
+        self.game_count = 0
 
-        self.results["computer"] = list()
-        self.results["me"] = list()
-        self.results["result"] = list()
-
-        self.results["computer"].append(observation)
-
-        return observation
+        return self.observation
     
     def step(self, action):
         self.me = action 
@@ -47,19 +42,17 @@ class RockScissorsPaper(gym.Env):
         elif [self.me, self.computer] in self.victory : reward = 2 # 우승
         elif [self.me, self.computer] in self.fail : reward = 0 # 패배
 
-        observation = random.random(0, 4) # 다음 step에 컴퓨터가 낼 손 모양
+        observation = random.randrange(0, 3) # 다음 step에 컴퓨터가 낼 손 모양
         
         self.game_count += 1 # 게임 횟수 증가
 
-        self.results["computer"].append(observation)
-        self.results["me"].append(self.me)
-        self.results["result"].append(reward)
-
         # 게임 횟수가 20번 이상이라면 에피소드 종료
-        if self.game_count >= GAME_END : done = False
-        else : done = True
+        if self.game_count >= GAME_END : 
+            done = True
+        else : 
+            done = False
 
-        pd.DataFrame(self.results).to_csv("results" + str(self.episode_count)+".csv")
+        #print(self.results)        
 
         return observation, reward, done, info
 
@@ -67,4 +60,3 @@ if __name__ == "__main__" :
     trainer = dqn.DQNTrainer(env=RockScissorsPaper)
     while True:
         trainer.train()
-
